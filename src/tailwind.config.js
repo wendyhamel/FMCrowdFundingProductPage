b68e6118d17b23aa3203e1fc9e67e04f33cd66e6
wendyhamel <whamel@it4web.nl>
2024-04-22 18:24:00 +0200

    Styling responsive large screens. Working on modal responsive styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,7 +10,11 @@ tailwind.config = {
 				'gray': 'hsl(0, 0%, 48%)',
 			},
 			fontSize: {
+				'2.5xl': ['1.75rem', {lineHeight: '2.5rem'}],
 				'3.5xl': ['2rem', {lineHeight: '2.5rem'}],
+			},
+			lineHeight: {
+				'7.5': '1.8rem'
 			}
 		}
 	}
